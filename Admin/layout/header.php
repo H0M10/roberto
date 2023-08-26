9922fd75fa4b016aaba74d38d5a7a067fb4899b5
Mi-Owen <owenarredondo17@gmail.com>
2023-08-25 22:23:55 -0600

    inicio de reportes

--- Admin/layout/header.php
+++ Admin/layout/header.php
@@ -57,6 +57,10 @@
                             <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                             Dashboard
                         </a>
+                        <a class="nav-link" href="../Otros/reporteventas.php">
+                            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+                            Reporte Ventas
+                        </a>
                         <div class="sb-sidenav-menu-heading">Gestion</div>
                         <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
                             <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
